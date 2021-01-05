94666bd612bbfe557d01c17392b07bb1fc2eb293
Marcos Nascimento <marcozzymandias@gmail.com>
2021-01-05 12:54:44 -0300

    Inicio da aula 03.Arrays, Loop e Estilos

--- js/principal.js
+++ js/principal.js
@@ -1,8 +1,33 @@
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
 
+const paciente = document.querySelector("#primeiro-paciente");
 
-var mensagem = "Seja bem vindo ao meu site!"
+const tdAltura = paciente.querySelector(".info-altura");
+const tdPeso = paciente.querySelector(".info-peso");
+const tdImc = paciente.querySelector(".info-imc");
 
-alert(mensagem);
+const altura = tdAltura.textContent;
+const peso = tdPeso.textContent;
+
+let alturaEhValida = true;
+let pesoEhValido = true;
+
+if (peso <= 0 || peso >= 1000) {
+  console.log("Peso inválido");
+  pesoEhValido = false;
+  tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.0) {
+  console.log("Altura inválida!");
+  alturaEhValida = false;
+  tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhValida && pesoEhValido) {
+  const imc = peso / (altura * altura);
+  tdImc.textContent = imc;
+} else {
+  tdImc.textContent = "Altura e/ou peso inválidos!"
+}
